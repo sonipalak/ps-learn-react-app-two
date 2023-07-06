c284bc27a8dac02410165be7b9f5e7d609673a18
sonipalak <online.palak@gmail.com>
2023-07-06 15:26:39 +0530

    Update

--- src/components/News.js
+++ src/components/News.js
@@ -67,7 +67,19 @@ export default class News extends Component {
         return (
             <div>
                 <h1>News List</h1>
-                <p onClick={this.viewToggle}>{buttonClassName}</p>
+                <a href="\#" className={`${buttonClassName}-view view-icon`} onClick={this.viewToggle}>
+                    <svg className='list-icon' width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="4" y="4" width="16" height="16" rx="3" stroke="#585DCD" strokeWidth="2"/>
+                        <path d="M16 10L8 10" stroke="#585DCD" strokeWidth="2" strokeLinecap="round"/>
+                        <path d="M16 14L8 14" stroke="#585DCD" strokeWidth="2" strokeLinecap="round"/>
+                    </svg>
+                    <svg className='grid-icon' width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 8.976C3 4.05476 4.05476 3 8.976 3H15.024C19.9452 3 21 4.05476 21 8.976V15.024C21 19.9452 19.9452 21 15.024 21H8.976C4.05476 21 3 19.9452 3 15.024V8.976Z" stroke="#585DCD" strokeWidth="2"/>
+                        <path d="M12 3V21" stroke="#585DCD" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M21 12L3 12" stroke="#585DCD" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                    {buttonClassName} View
+                </a>
                 {this.state.loading && <Loading />}
                 <div className={`${buttonClassName}-view`}>
                     {this.state.articles.map((element) => {
